55ae1c5586ea2adb6ce9b5c9090745f19a4ebc7a
Komil Sobitov <148330360+ksobitov@users.noreply.github.com>
2024-03-04 15:17:45 +0500

    Update kubernetes_setup.sh

--- server/kubernetes_setup.sh
+++ server/kubernetes_setup.sh
@@ -58,7 +58,7 @@ function setup_docker {
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
 
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
     echo "Docker is now snugly set up in their new home. 🏠✨"
 }
 
